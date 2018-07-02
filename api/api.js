1d0b15ff5c27baafcfbabead025b955b1c74741d
Alex Smith <alexsmith716@outlook.com>
2018-07-02 14:12:23 -0400

    mon-july-two-only-comments-added-one

--- api/api.js
+++ api/api.js
@@ -1,3 +1,5 @@
+
+// The '@feathersjs/express' module contains Express framework integrations for Feathers
 import express from '@feathersjs/express';
 import feathers from '@feathersjs/feathers';
 import socketio from '@feathersjs/socketio';
@@ -14,41 +16,77 @@ process.on('unhandledRejection', (error, promise) => {
   console.error('>>>>>> API > API > Unhandled Rejection at:', promise, 'reason:', error);
 });
 
+// *********************************************************************************************
+// Configure Express server and REST API
+// Turn this Feathers application into a Express compatible application
+// Additionally to Feathers functionality also lets you use the Express API
+// *********************************************************************************************
+
+// Create the app that is a Feathers AND Express application
 const app = express(feathers());
 
+// assign setting name 'config' to value config object 'auth' (cookie info)
 app.set('config', config);
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieParser()); // parse cookie header and populate req.cookies
 
 
-app.use((req, res, next) => {
-  console.log('>>>>>>>>>>>>>>>>> API > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ IN > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
-  // console.log('>>>>>>>>>>>>>>>>> API > REQ.ip +++++++++++++: ', req.ip);
-  console.log('>>>>>>>>>>>>>>>>> API > REQ.method +++++++++: ', req.method);
-  console.log('>>>>>>>>>>>>>>>>> API > REQ.url ++++++++++++: ', req.url);
-  console.log('>>>>>>>>>>>>>>>>> API > REQ.headers ++++++++: ', req.headers);
-  // console.log('>>>>>>>>>>>>>>>>> API > REQ.session ++++++++: ', req.session);
-  // console.log('>>>>>>>>>>>>>>>>> API > REQ.params +++++++++: ', req.params);
-  // console.log('>>>>>>>>>>>>>>>>> API > REQ.originalUrl ++++: ', req.originalUrl);
-  console.log('>>>>>>>>>>>>>>>>> API > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ IN < $$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
-  return next();
-});
+// EXPRESS-SESSION:
+
+// resave: false,             // don't save session if unmodified
+                              // Forces session to be saved back to the session store, 
+                              // even if the session was never modified during the request.
 
 // saveUninitialized: false,  // don't create session until something stored
-// resave: false,             // don't save session if unmodified
+                              // Forces 'uninitialized' session to be saved to the store
+                              // A session is uninitialized when it is new but not modified
 
+
+// *** Note:
+// *** if you are using Session in conjunction with PassportJS, 
+// *** Passport will add an empty Passport object to the session for use after a user is authenticated, 
+// *** which will be treated as a modification to the session, causing it to be saved. 
+
+// creating session 
 app.use(session({
-  secret: serverConfig.sessionSecret,
+  secret: serverConfig.sessionSecret, // secret used to sign the session ID cookie
   resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 60000 }
 }));
 
+
+// *********************************************************************************************
+// Configure Exposing services through this RESTful API
+// *********************************************************************************************
+
+// parse data in the HTTP body
+// so REST can .create, .update and .patch calls parse the data in the HTTP body
+// Turn on URL-encoded parser for REST services
 app.use(bodyParser.urlencoded({ extended: true }));
+// Turn on JSON parser for REST services
 app.use(bodyParser.json());
 
+// Set up REST transport
 app.configure(express.rest());
+
+// Set up real-time socket transport
 app.configure(socketio({ path: '/ws' }));
+
+app.use((req, res, next) => {
+  console.log('>>>>>>>>>>>>>>>>> API > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ IN > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+  // console.log('>>>>>>>>>>>>>>>>> API > REQ.ip +++++++++++++: ', req.ip);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.method +++++++++++++++: ', req.method);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.url ++++++++++++++++++: ', req.url);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.headers ++++++++++++++: ', req.headers);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.cookies ++++++++++++++: ', req.cookies);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.signedCookies ++++++++: ', req.signedCookies);
+    console.log('>>>>>>>>>>>>>>>>> API > REQ.session ++++++++: ', req.session);
+  // console.log('>>>>>>>>>>>>>>>>> API > REQ.params +++++++++: ', req.params);
+  // console.log('>>>>>>>>>>>>>>>>> API > REQ.originalUrl ++++: ', req.originalUrl);
+  console.log('>>>>>>>>>>>>>>>>> API > $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ IN < $$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+  return next();
+});
 
 app.configure(services);
 app.configure(channels);
--- bin/server.js
+++ bin/server.js
@@ -1,4 +1,6 @@
 
+// enable ES6 and ES7
+
 require('../server.babel');
 
 const path = require('path');
@@ -10,4 +12,5 @@ global.__DISABLE_SSR__ = false;
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 global.__DLLS__ = process.env.WEBPACK_DLLS === '1';
 
+// initiates 'server.js'
 require('../server/index');
